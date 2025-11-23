a47bfd049a7e65309117dbb58319901a22e5eae2
saeidelkhouly <saeidelkhouly@gmail.com>
2025-11-23 23:11:28 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BudgetTrackerApp());
+}
+
+class BudgetTrackerApp extends StatelessWidget {
+  const BudgetTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('بسم الله توكلت علي الله'))),
+    );
+  }
+}
